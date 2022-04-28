d56815c089771783f6c40cf03784017cb36f5e64
David Hall <104573661+d460h434@users.noreply.github.com>
2022-04-28 11:06:52 -0500

    Update example.py

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ HR_file = st.file_uploader("This is where users would upload HR data files store
 maximo_file = st.file_uploader("And this is where they would upload the maximo space data files",type='csv')
 if HR_file is not None:
     st.write('Thank you for uploading your HR data file!')
-    variable = st.multiselect('select a department',list(HR_file.dept.unique())
+    variable = st.multiselect('select a department',list(HR_file.dept.unique()))
     variable_two = st.selectbox('Pick a name',list(HR_file.person.unique()))
 
 if maximo_file is not None:
