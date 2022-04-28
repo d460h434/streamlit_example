619d96b28d4c3f62dfaf0692ce2346898b35e07d
David Hall <104573661+d460h434@users.noreply.github.com>
2022-04-28 14:06:26 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,26 +1,58 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
 
-#main code
-st.title("Hello! this is where the space management app would be")
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-HR_file = st.file_uploader("This is where users would upload HR data files stored locally on their machine or in approved KU locations",type='csv',)
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
 
-maximo_file = st.file_uploader("And this is where they would upload the maximo space data files",type='csv')
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
 
-if HR_file is not None:
-    HR_file=pd.read_csv(HR_file)
-    st.write('Thank you for uploading your HR data file!')
-    column = st.selectbox("Select a column you would like to pick and value from",list(HR_file.columns.values))
-    if column is not None:
-        variable = st.multiselect('select a department',list(HR_file.loc[:,column]))
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
+    #main code
+    st.title("Hello! this is where the space management app would be")
 
-if maximo_file is not None:
-    maximo_file=pd.read_csv(maximo_file)
-    st.write('Thank you for uploading your maximo data file!')
-    column_two = st.selectbox("Select a column you would like to pick and value from",list(maximo_file.columns.values))
-    if column_two is not None:
-        variable_two = st.multiselect('select a department',list(maximo_file.loc[:,column_two]))
+    HR_file = st.file_uploader("This is where users would upload HR data files stored locally on their machine or in approved KU locations",type='csv',)
+
+    maximo_file = st.file_uploader("And this is where they would upload the maximo space data files",type='csv')
+
+    if HR_file is not None:
+        HR_file=pd.read_csv(HR_file)
+        st.write('Thank you for uploading your HR data file!')
+        column = st.selectbox("Select a column you would like to pick and value from",list(HR_file.columns.values))
+        if column is not None:
+            variable = st.multiselect('select a department',list(HR_file.loc[:,column]))
+
+    if maximo_file is not None:
+        maximo_file=pd.read_csv(maximo_file)
+        st.write('Thank you for uploading your maximo data file!')
+        column_two = st.selectbox("Select a column you would like to pick and value from",list(maximo_file.columns.values))
+        if column_two is not None:
+            variable_two = st.multiselect('select a department',list(maximo_file.loc[:,column_two]))
 
 
 
