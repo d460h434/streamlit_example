c0ff36f4e38ee0decf95247415744ff6cbfc5e40
David Hall <104573661+d460h434@users.noreply.github.com>
2022-04-29 14:57:11 -0500

    Update example.py

--- example.py
+++ example.py
@@ -39,7 +39,7 @@ def check_password():
 
 if check_password():
     #main code
-    st.video(data='<iframe id="kmsembed-1_fmtsbmgc" width="400" height="285" src="https://mediahub.ku.edu/embed/secure/iframe/entryId/1_fmtsbmgc/uiConfId/45147491/st/0" class="kmsembed" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" referrerPolicy="no-referrer-when-downgrade" sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Kaltura Player"></iframe>')
+    st.video(data='https://www.youtube.com/watch?v=0hu0npYVcg0')
     st.title("Hello! this is where the space management app would be")
 
     HR_file = st.file_uploader("This is where users would upload HR data files stored locally on their machine or in approved KU locations",type='csv',)
